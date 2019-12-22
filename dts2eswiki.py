c0525f11b05f3dddb366968c312b49a1b65a9d79
Davod <davidkingnt@gmail.com>
2019-12-21 23:03:23 -0300

    Create dts2eswiki.py

---
+++ dts2eswiki.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import re
+import sys
+import getopt
+
+def callback(match):
+	import timestring
+	date_ob = timestring.Date(str(match))
+	dts = "{{dts|" + str(date_ob.day) + "|" + str(date_ob.month) + "|" + str(date_ob.year) + "}}"
+	return dts
+
+try:
+	filename = sys.argv[1]
+except IndexError:
+	print('Please provide a valid filename to be parsed')
+	exit(1)
+
+try:
+	data = open(filename, 'r', encoding="utf8").read()
+except FileNotFoundError as e:
+	print(e)
+	exit(1)
+
+data = re.sub('{{(dts|Dts)\|[\w, \|]+}}', callback, data)
+
+with open(filename, 'w', encoding="utf8") as f:
+    f.write(data)
